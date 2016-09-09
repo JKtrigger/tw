51d214a511269d7606a59312ba0adfc7333e0cbd
trigger <kasher89@mail.ru>
2016-09-09 19:34:54 +0200

    very inpotand way

--- tw/litleTwit/models.py
+++ tw/litleTwit/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
-
+#some 
 class Message(models.Model):
     body  =   models.CharField        (max_length=140,blank=False,verbose_name='body')
     owner =   models.ForeignKey       (User,verbose_name='UIN')
